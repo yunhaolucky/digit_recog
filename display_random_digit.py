c05ba558916c1b421705db5f2b5226c0419818fa
Yun Hao <yunhao.lucky@gmail.com>
2014-08-06 10:20:33 -0600

    add ipython notebook

--- display_random_digit.py
+++ display_random_digit.py
@@ -18,18 +18,19 @@ row=3
 col=3
 df = pd.read_csv('./train.csv')
 #df = df.astype('float64')
-f,  l= plt.subplots(ncols=col,nrows=row)
-#l=(ax1, ax2,ax3,ax4)
+for n in range(5):
+	f,  l= plt.subplots(ncols=col,nrows=row)
+	#l=(ax1, ax2,ax3,ax4)
 
-imsize = (28, 28)
+	imsize = (28, 28)
 
-for i in range(row):
-	for j in range(col):
-		l[i][j].matshow(np.reshape(df.ix[random.randint(0,df.shape[0]),1:], imsize), cmap='gray_r')
-		l[i][j].axis('off')
+	for i in range(row):
+		for j in range(col):
+			l[i][j].matshow(np.reshape(df.ix[random.randint(0,df.shape[0]),1:], imsize), cmap='gray_r')
+			l[i][j].axis('off')
 
-f.tight_layout();
+	#f.tight_layout();
 
-#plt.imshow(f)
-plt.show()
+	#plt.imshow(f)
+	plt.show()
 
